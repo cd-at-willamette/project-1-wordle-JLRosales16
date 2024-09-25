3db725bf16b3b94bcf3db370a6093f561e9c27b8
JLRosales16 <jlrosales@willamette.edu>
2024-09-24 21:31:11 -0700

    Add files via upload

--- Wordle.py
+++ Wordle.py
@@ -1,5 +1,5 @@
 ########################################
-# Name:
+# Name: Jacob Rosales
 # Collaborators (if any):
 # GenAI Transcript (if any):
 # Estimated time spent (hr):
@@ -17,9 +17,25 @@ def wordle():
         # What should happen when RETURN/ENTER is pressed.
         gw.show_message("You need to implement this method")
 
+        if word_to_row in ENGLISH_WORDS:  
+            gw.show_message("confirmed")
+        else:
+            gw.show_message("not in word list")
+
+        
+    def word_to_row(row:int, word:str):
+        gw.show_message("words")
 
     gw = WordleGWindow()
     gw.add_enter_listener(enter_action)
+    gw.set_square_letter(0,0,'q')
+    gw.set_square_letter(0,1,'u')
+    gw.set_square_letter(0,2,'a')
+    gw.set_square_letter(0,3,'k')
+    gw.set_square_letter(0,4,'e')
+    
+
+    
 
 
 
